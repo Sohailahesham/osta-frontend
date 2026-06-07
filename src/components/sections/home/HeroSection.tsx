09c6dfc754f64f813a1dde9d35fe2abdeeab232a
Hajar Zain <hajarzain222@gmail.com>
2026-06-07 18:20:22 +0300

    Add user main flow

--- src/components/sections/home/HeroSection.tsx
+++ src/components/sections/home/HeroSection.tsx
@@ -6,14 +6,13 @@ import heroImage from "@/assets/images/hero-bg.png";
 export default function HeroSection() {
   return (
     <section className="">
-        <h1>مرحبا بك ف اوسطي</h1>
-        <div className="w-full max-w-md">
+        <div className="">
           <Image
             src={heroImage}
             alt="Hero"
             width={500}
             height={500}
-            className="w-full h-auto object-contain"
+            className=""
           />
         </div>
     </section>
